0ce175a8726ecb93d60185cdc094925349334481
gumquat <gumquat@gmail.com>
2024-05-02 14:09:14 -0500

    starting task 4 (aka task_3 file)

---
+++ react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+import holbertonLogo from './holberton-logo.jpg'; // Import the Holberton logo
+import { Notifications } from './Notifications';
+
+
+/* in-file webpage footer module */
+const footerContent = () => {
+  return (
+    <footer className="App-footer">
+      <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+  )
+}
+
+  /* WEB PAGE */
+  function App() {
+    return (
+      <div className="App">
+        {Notifications()}
+        <header className="App-header">
+          <img src={holbertonLogo} className="App-logo" alt="logo" />
+          <h1>
+            School dashboard
+          </h1>
+        </header>
+        <body className="App-body">
+        <p>Login to access the full dashboard</p>
+          <div className='login-form'>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" />
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" />
+            <button>OK</button>
+          </div>
+        </body>
+
+        {footerContent()}
+      </div>
+    );
+  }
+
+export default App;
---
+++ react_intro/task_3/dashboard/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          'react-hot-loader/babel', // Add this line
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    hot: true, // Enable hot module replacement
+    liveReload: true, // Enable live reloading
+  },
+};
