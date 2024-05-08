cb343f15105b97b66bc1edb2ce2706b1d9b581c0
gumquat <gumquat@gmail.com>
2024-05-08 11:40:02 -0500

    task 0 part 0 complete

--- React_component/task_0/dashboard/src/App/App.js
+++ React_component/task_0/dashboard/src/App/App.js
@@ -7,42 +7,48 @@ import Login from '../Login/Login';
 import CourseList from '../CourseList/CourseList';
 import PropTypes from 'prop-types';
 
-function App({ isLoggedIn }) {
-  // const [isLoggedIn] = React.useState(false);
-  const listCourses = [
-    { id: 1, name: 'ES6', credit: 60 },
-    { id: 2, name: 'Webpack', credit: 20 },
-    { id: 3, name: 'React', credit: 40 },
-  ];
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const listNotifications = [
-    { id: 1, type: 'default', value: 'New course available' },
-    { id: 2, type: 'urgent', value: 'New resume available' },
-    { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } },
-  ]
+    this.listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
 
-  return (
-    <>
-      <Notifications />
-      <div className="App">
-        <Header />
-        <Notifications listNotifications={listNotifications} />
-        <body>
-          <div className="App-body">
-            {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
-          </div>
-        </body>
-        <Footer />
-      </div>
-    </>
-  );
+    this.listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } },
+    ];
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+
+    return (
+      <>
+        <Notifications />
+        <div className="App">
+          <Header />
+          <Notifications listNotifications={this.listNotifications} />
+          <body>
+            <div className="App-body">
+              {isLoggedIn ? <CourseList listCourses={this.listCourses} /> : <Login />}
+            </div>
+          </body>
+          <Footer />
+        </div>
+      </>
+    );
+  }
 }
 
-// is a bool...
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
 };
-// ...defautls to false
+
 App.defaultProps = {
   isLoggedIn: false,
 };
