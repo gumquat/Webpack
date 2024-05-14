47cf237cdc7defa052e1b62bacd1973ac1c376e6
gumquat <gumquat@gmail.com>
2024-05-14 13:41:44 -0500

    Modifications to Header

--- React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import holbertonLogo from '../assets/holbertonLogo.jpg';
-import './Header.css';
+import { StyleSheet, css } from 'aphrodite';
 
+const styles = StyleSheet.create({
+  header: {
+    textAlign: 'center',
+    backgroundColor: '#282c34',
+    padding: '20px',
+    color: 'white',
+  },
+  logo: {
+    width: '200px',
+    height: 'auto',
+  },
+});
 
 function Header() {
   return (
-    <header className="App-header">
-        <img src={holbertonLogo} className="App-logo" alt="logo" />
-        <h1>
-          School dashboard
-        </h1>
+    <header className={css(styles.header)}>
+      <img src={holbertonLogo} className={css(styles.logo)} alt="logo" />
+      <h1>
+        School dashboard
+      </h1>
     </header>
   );
 }
