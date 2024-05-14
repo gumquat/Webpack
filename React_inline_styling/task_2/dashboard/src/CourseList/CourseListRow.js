255328ac4946570e86bcb4d07d2452688e5b88b8
gumquat <gumquat@gmail.com>
2024-05-14 16:59:47 -0500

    modified CourseListRow and its test

--- React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
+++ React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  headerRow: {
+    backgroundColor: '#deb5b545',
+  },
+  defaultRow: {
+    backgroundColor: '#f5f5f5ab',
+  },
+  th: {
+    fontWeight: 'bold',
+    padding: '8px',
+  },
+});
 
 function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
   const CellType = isHeader ? 'th' : 'td';
-  const rowStyle = isHeader ? { backgroundColor: '#deb5b545' } : { backgroundColor: '#f5f5f5ab' };
+  const rowStyle = isHeader ? styles.headerRow : styles.defaultRow;
+  const cellStyle = isHeader ? styles.th : null;
 
   return (
-    <tr style={rowStyle}>
+    <tr className={css(rowStyle)}>
       {textSecondCell === null ? (
-        <CellType colSpan={isHeader ? "2" : undefined}>{textFirstCell}</CellType>
+        <CellType colSpan={isHeader ? "2" : undefined} className={css(cellStyle)}>{textFirstCell}</CellType>
       ) : (
         <>
-          <CellType>{textFirstCell}</CellType>
-          <CellType>{textSecondCell}</CellType>
+          <CellType className={css(cellStyle)}>{textFirstCell}</CellType>
+          <CellType className={css(cellStyle)}>{textSecondCell}</CellType>
         </>
       )}
     </tr>
