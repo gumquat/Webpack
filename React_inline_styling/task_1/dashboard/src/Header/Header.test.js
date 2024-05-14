53c719e85cbfc551dbf06693cf14855b7e6cd368
gumquat <gumquat@gmail.com>
2024-05-14 15:40:41 -0500

    SUPRRESS STYLE INJECTIOINS

--- React_inline_styling/task_1/dashboard/src/Header/Header.test.js
+++ React_inline_styling/task_1/dashboard/src/Header/Header.test.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Header from './Header';
+import { StyleSheetTestUtils } from 'aphrodite';
 
 describe('Header', () => {
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
   it('renders without crashing', () => {
     shallow(<Header />);
   });
 
   it('renders h1 tag', () => {
     const wrapper = shallow(<Header />);
-    expect(wrapper.find('h1').length).toBe(1);
+    expect(wrapper.find('h1').exists()).toBe(true);
   });
 
   it('renders img tag', () => {
     const wrapper = shallow(<Header />);
-    expect(wrapper.find('img').length).toBe(1);
+    expect(wrapper.find('img').exists()).toBe(true);
   });
-});
+})
