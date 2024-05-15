6bf06aa9c1c44230b5568531ee9139136729c855
gumquat <gumquat@gmail.com>
2024-05-15 09:43:44 -0500

    modificatiosn to Notifications

--- React_inline_styling/task_2/dashboard/src/Login/Login.js
+++ React_inline_styling/task_2/dashboard/src/Login/Login.js
@@ -10,38 +10,50 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     top: '3rem',
     left: '3rem',
+    '@media (max-width: 900px)': {
+      alignItems: 'center',
+    },
   },
   loginParagraph: {
     fontWeight: '400',
     fontSize: '1.5rem',
     margin: '4rem 2rem 2rem 4rem',
   },
+  labelInput: {
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: '0.5rem',
+  },
   label: {
     paddingRight: '0.5rem',
   },
   input: {
-    marginRight: '1rem',
     padding: '0.5rem',
     border: '1px solid #000',
     borderRadius: '4px',
   },
   button: {
     padding: '0.5rem 1rem',
+    marginTop: '1rem',
   },
 });
 
 function Login() {
   return (
-      <div className={css(styles.loginForm)}>
-        <p className={css(styles.loginParagraph)}>
-          Login to access the full dashboard
-        </p>
-          <label className={css(styles.label)} htmlFor="email" >Email:</label>
-          <input className={css(styles.input)} type="text" id="email" name="email" />
-          <label className={css(styles.label)} htmlFor="password" >Password:</label>
-          <input className={css(styles.input)} type="password" id="password" name="password" />
-          <button className={css(styles.button)} type="submit" >OK</button>
+    <div className={css(styles.loginForm)}>
+      <p className={css(styles.loginParagraph)}>
+        Login to access the full dashboard
+      </p>
+      <div className={css(styles.labelInput)}>
+        <label className={css(styles.label)} htmlFor="email">Email:</label>
+        <input className={css(styles.input)} type="text" id="email" name="email" />
       </div>
+      <div className={css(styles.labelInput)}>
+        <label className={css(styles.label)} htmlFor="password">Password:</label>
+        <input className={css(styles.input)} type="password" id="password" name="password" />
+      </div>
+      <button className={css(styles.button)} type="submit">OK</button>
+    </div>
   );
 }
 
