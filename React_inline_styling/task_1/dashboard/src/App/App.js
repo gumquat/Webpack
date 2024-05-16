7b96a52927e1399d020e9d963d5637bbd7d284c6
gumquat <gumquat@gmail.com>
2024-05-16 09:23:11 -0500

    removed comment for the checker

--- React_inline_styling/task_1/dashboard/src/App/App.js
+++ React_inline_styling/task_1/dashboard/src/App/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import './App.css';
 import Notifications from "../Notifications/Notifications";
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
