7a2df5ed8527ce98237f126aa9ca89d65037d1eb
gumquat <gumquat@gmail.com>
2024-05-28 11:19:13 -0500

    begin task 2

---
+++ react_redux_reducer_selector/task_2/dashboard/src/reducers/uiReducer.js
@@ -0,0 +1,41 @@
+import initialState from './uiReducer';
+import { Map } from 'immutable';
+import {
+  LOGIN,
+  LOGOUT,
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE
+} from './actions/uiActionTypes';
+
+const initialState = Map({
+  isNotificationDrawerVisible: false,
+  isUserLoggedIn: false,
+  user: Map()
+});
+
+const uiReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case DISPLAY_NOTIFICATION_DRAWER:
+      return state.set('isNotificationDrawerVisible', true);
+    case HIDE_NOTIFICATION_DRAWER:
+      return state.set('isNotificationDrawerVisible', false);
+    case LOGIN_SUCCESS:
+      return state
+        .set('isUserLoggedIn', true)
+        .set('user', Map(action.payload.user || {}));
+    case LOGIN_FAILURE:
+      return state
+        .set('isUserLoggedIn', false)
+        .set('user', Map());
+    case LOGOUT:
+      return state
+        .set('isUserLoggedIn', false)
+        .set('user', Map());
+    default:
+      return state;
+  }
+};
+
+export default uiReducer;
