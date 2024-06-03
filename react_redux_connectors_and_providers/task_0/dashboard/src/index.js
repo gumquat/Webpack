0c93033d87c8004842dcfeb7e1a9941777f502da
gumquat <gumquat@gmail.com>
2024-06-03 13:14:29 -0500

    task 7

--- react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -4,8 +4,12 @@ import App from './App/App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import uiReducer from 'reducers/uiReducer';
+import thunk from 'redux-thunk';
 
-const store = createStore(uiReducer);
+const store = createStore(
+  uiReducer,
+  applyMiddleware(thunk)
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
