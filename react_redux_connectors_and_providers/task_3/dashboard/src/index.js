c10c2b3eca0527bf688eaeaa258957a3a536b4ab
gumquat <gumquat@gmail.com>
2024-06-04 14:24:36 -0500

    tas k15

--- react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import uiReducer from './reducers/uiReducer';
-import { thunk } from 'redux-thunk';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers/rootReducer';
 
 const composeEnhancers = composeWithDevTools({
-  // Specify here if you want to disable the extension in some environments
-  // e.g., /* disable extension in production */
-  // process.env.NODE_ENV === 'production' ? undefined : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
 }) || window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
 const store = createStore(
-  uiReducer,
+  rootReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
 
