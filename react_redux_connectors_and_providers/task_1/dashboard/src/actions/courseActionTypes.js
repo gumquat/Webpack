539a757dd492d48e4e1876a67ce9df797eb0d81e
gumquat <gumquat@gmail.com>
2024-06-04 12:01:30 -0500

    added courses file lol

--- react_redux_connectors_and_providers/task_1/dashboard/src/actions/courseActionTypes.js
+++
@@ -1,5 +0,0 @@
-// courseActionTypes.js
-
-export const SELECT_COURSE = 'SELECT_COURSE';
-export const UNSELECT_COURSE = 'UNSELECT_COURSE';
-export const FETCH_COURSE_SUCCESS = 'FETCH_COURSE_SUCCESS';
--- react_redux_connectors_and_providers/task_1/dashboard/src/schema/courses.js
+++
@@ -1,8 +0,0 @@
-import { schema, normalize } from 'normalizr';
-
-// Define a schema for the course entity
-const course = new schema.Entity('courses', {}, { idAttribute: 'id' });
-// Define a schema for the courses list
-const courses = new schema.Array(course);
-// Function to normalize courses data
-export const coursesNormalizer = (data) => normalize(data, courses);
