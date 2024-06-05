20c654a75ef8bad90eb52500a319542cb9d40389
gumquat <gumquat@gmail.com>
2024-06-05 11:19:37 -0500

    task 18

--- react_redux_connectors_and_providers/task_5/dashboard/src/reducers/notificationReducer.js
+++ react_redux_connectors_and_providers/task_5/dashboard/src/reducers/notificationReducer.js
@@ -3,6 +3,7 @@ import {
   MARK_AS_READ,
   SET_TYPE_FILTER,
   FETCH_NOTIFICATIONS_SUCCESS,
+  SET_LOADING_STATE,
   NotificationTypeFilters,
 } from './notificationActionTypes';
 import { notificationsNormalizer } from './notifications';
@@ -11,16 +12,24 @@ const initialState = Map({
   filter: NotificationTypeFilters.DEFAULT,
   entities: Map(),
   result: [],
+  loading: false, // Add loading attribute to the initial state
 });
 
 const notificationReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SET_LOADING_STATE:
+      return state.set('loading', action.loading);
     case FETCH_NOTIFICATIONS_SUCCESS: {
       const normalizedData = notificationsNormalizer(action.data);
-      return state.mergeDeep(fromJS({
-        entities: normalizedData.entities,
-        result: normalizedData.result,
-      }));
+      return state.mergeDeep(
+        fromJS({
+          entities: {
+            notifications: normalizedData.entities.notifications,
+          },
+          result: normalizedData.result,
+          loading: false, // Set loading to false after successful fetch
+        })
+      );
     }
     case MARK_AS_READ:
       return state.setIn(['entities', 'notifications', action.notificationId, 'isRead'], true);
