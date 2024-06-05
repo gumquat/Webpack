30f4525c1758aa4d2ddafdff3a8d0104abf50663
gumquat <gumquat@gmail.com>
2024-06-05 11:24:52 -0500

    task 20

--- react_redux_connectors_and_providers/task_5/dashboard/src/App/App.js
+++ react_redux_connectors_and_providers/task_5/dashboard/src/App/App.js
@@ -74,12 +74,6 @@ const App = ({
     { id: 3, name: 'React', credit: 40 },
   ];
 
-  const listNotifications = [
-    { id: 1, type: 'default', value: 'New course available' },
-    { id: 2, type: 'urgent', value: 'New resume available' },
-    { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } },
-  ];
-
   // Initialize the component state
   const [user, setUser] = useState({
     email: '',
